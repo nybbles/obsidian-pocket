50701e5d1437e2be0d77be6711db82e1a2b16067
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2021-08-27 16:37:02 -0700

    Faster persistence of multiple Pocket item updates by using a single transaction

--- src/PocketItemStore.ts
+++ src/PocketItemStore.ts
@@ -1,4 +1,4 @@
-import { IDBPDatabase, openDB } from "idb";
+import { IDBPDatabase, IDBPObjectStore, openDB } from "idb";
 import log from "loglevel";
 import { Notice } from "obsidian";
 import { UpdateTimestamp } from "./PocketAPI";
@@ -21,9 +21,19 @@ const LAST_UPDATED_TIMESTAMP_KEY = "last_updated_timestamp";
 export type OnChangeCallback = () => Promise<void>;
 export type CallbackId = string;
 
+type IDBPPocketItemStoreRW = IDBPObjectStore<
+  unknown,
+  ["items"],
+  "items",
+  "readwrite"
+>;
+
 export class PocketItemStore {
   db: IDBPDatabase;
   onChangeCallbacks: Map<ViewName, OnChangeCallback>;
+
+  static isItemValid = (item: SavedPocketItem) =>
+    !item.resolved_title && !item.resolved_url;
 
   constructor(db: IDBPDatabase) {
     this.db = db;
@@ -34,23 +44,25 @@ export class PocketItemStore {
     lastUpdateTimestamp: UpdateTimestamp,
     items: PocketItemRecord
   ): Promise<void> => {
-    const updates = [];
-
     // TODO: Should all of this be happening in a transaction?
     log.debug("Applying updates to Pocket item store");
+
+    const tx = this.db.transaction(ITEM_STORE_NAME, "readwrite");
+
     for (const key in items) {
       const item = items[key];
       if (isDeletedPocketItem(item)) {
-        updates.push(this.deleteItem(item.item_id, false));
+        this.deleteItem(tx.store, item.item_id, false);
       } else if (isSavedPocketItem(item)) {
-        updates.push(this.putItem(item, false));
+        this.putItem(tx.store, item, false);
       } else {
         throw new Error("unexpected");
       }
     }
 
+    await tx.done;
+
     // Wait on all changes, update timestamp, then trigger registered onChange handlers
-    await Promise.all(updates);
     log.debug("Updates applied to Pocket item store");
     this.setLastUpdateTimestamp(lastUpdateTimestamp);
     log.debug("Running Pocket item store onChange handlers");
@@ -63,16 +75,17 @@ export class PocketItemStore {
   };
 
   putItem = async (
+    store: IDBPPocketItemStoreRW,
     item: SavedPocketItem,
     triggerOnChangeHandlers?: boolean
   ) => {
-    if (!item.resolved_title && !item.resolved_url) {
+    if (PocketItemStore.isItemValid(item)) {
       log.warn(
         `Item ${item.item_id} is invalid, not adding to Pocket item store`
       );
       return;
     }
-    await this.db.put(ITEM_STORE_NAME, item);
+    await store.put(item);
     triggerOnChangeHandlers && (await this.handleOnChange());
   };
 
@@ -89,10 +102,11 @@ export class PocketItemStore {
   };
 
   deleteItem = async (
+    store: IDBPPocketItemStoreRW,
     itemId: PocketItemId,
     triggerOnChangeHandlers?: boolean
   ) => {
-    await this.db.delete(ITEM_STORE_NAME, itemId);
+    await store.delete(itemId);
     triggerOnChangeHandlers && (await this.handleOnChange());
   };
 
