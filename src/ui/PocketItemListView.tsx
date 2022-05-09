4c8ef8f29e869b4df3210d67cb84d4d71ab0369c
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2022-05-09 15:58:33 -0700

    Fixing issue where resolved Pocket item notes were rendering the same way as unresolved ones

--- src/ui/PocketItemListView.tsx
+++ src/ui/PocketItemListView.tsx
@@ -31,7 +31,7 @@ export class PocketItemListView extends ItemView {
 
   getPortal() {
     return (
-      <div className={"markdown-preview-view"}>
+      <div className={"markdown-rendered"}>
         <PocketItemList
           itemStore={this.plugin.itemStore}
           metadataCache={this.app.metadataCache}
